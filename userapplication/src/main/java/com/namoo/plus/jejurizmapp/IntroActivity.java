70e3cd171a6f8461e92311e9f784279f6e09ed1e
kylee <kylee@devband.com>
2016-06-29 09:15:50 +0900

    intro 액티비티 추가

--- userapplication/src/main/java/com/namoo/plus/jejurizmapp/IntroActivity.java
+++ userapplication/src/main/java/com/namoo/plus/jejurizmapp/IntroActivity.java
@@ -1,7 +1,16 @@
 package com.namoo.plus.jejurizmapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by lee on 2016. 6. 29..
  */
-public class IntroActivity {
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
