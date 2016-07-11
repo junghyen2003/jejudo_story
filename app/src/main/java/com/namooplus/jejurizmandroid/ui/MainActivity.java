5976f92a759a2b72c55c5d599536920155a82459
HSL <zoo5252@naver.com>
2016-07-11 22:11:40 +0900

    작업중 임시

--- app/src/main/java/com/namooplus/jejurizmandroid/ui/MainActivity.java
+++ app/src/main/java/com/namooplus/jejurizmandroid/ui/MainActivity.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.namooplus.jejurizmandroid.R;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by HeungSun-AndBut on 2016. 6. 5..
@@ -15,12 +19,18 @@ import com.namooplus.jejurizmandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.activity_main_camera_sign_button)
+    private ImageButton mSignCamera;
 
+    @BindView(R.id.activity_main_camera_landscape_button)
+    private ImageButton mLandscapeCamera;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(MainActivity.this);
 
         Button btn = (Button)findViewById(R.id.activity_main_camera_button);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -31,4 +41,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
